4e06ca90ecd36af68794e3c9f120b9e960f2a9db
matiascastilloaudio <177944762+matiascastilloaudio@users.noreply.github.com>
2024-12-19 14:28:26 -0300

    feat: install prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Testing Practice
+
 The Odin Project - JavaScript Course
---
+++ main.js
@@ -0,0 +1,6 @@
+function capitalize(str) {
+    let firstLetter = str.charAt(0).toUpperCase() // M
+    return firstLetter
+}
+
+console.log(capitalize('miller'))
