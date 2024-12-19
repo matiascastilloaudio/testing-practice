087077c552f6bec6722ce8c7f25e591502139662
matiascastilloaudio <177944762+matiascastilloaudio@users.noreply.github.com>
2024-12-19 15:37:58 -0300

    submit exercise

--- main.js
+++ main.js
@@ -9,4 +9,59 @@ function reverseString(str) {
     return revStr;
 }
 
-export { capitalize, reverseString };
+const calculator = (() => {
+    function add(a, b) {
+        return a + b;
+    }
+    function subtract(a, b) {
+        return a - b;
+    }
+    function divide(a, b) {
+        return a / b;
+    }
+    function multiply(a, b) {
+        return a * b;
+    }
+    return { add, subtract, divide, multiply };
+})();
+
+function caesarCipher(str, num) {
+    num = num % 26;
+    let result = '';
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        let charCode = char.charCodeAt(0);
+
+        if (charCode >= 65 && charCode <= 90) {
+            result += String.fromCharCode(
+                ((charCode - 65 + num + 26) % 26) + 65
+            );
+        } else if (charCode >= 97 && charCode <= 122) {
+            result += String.fromCharCode(
+                ((charCode - 97 + num + 26) % 26) + 97
+            );
+        } else {
+            result += char;
+        }
+    }
+    return result;
+}
+
+function analyzeArray(arr) {
+    if (arr.length === 0) {
+        return { average: 0, min: 0, max: 0, length: 0 };
+    }
+    const sum = arr.reduce((acc, num) => acc + num, 0);
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+
+    return {
+        average: sum / arr.length,
+        min: min,
+        max: max,
+        length: arr.length,
+    };
+}
+
+export { capitalize, reverseString, calculator, caesarCipher, analyzeArray };
--- main.test.js
+++ main.test.js
@@ -1,4 +1,10 @@
-import { capitalize, reverseString } from './main.js';
+import {
+    capitalize,
+    reverseString,
+    calculator,
+    caesarCipher,
+    analyzeArray,
+} from './main.js';
 
 test('Capitalize first letter of a string', () => {
     expect(capitalize('apple')).toBe('Apple');
@@ -6,4 +12,27 @@ test('Capitalize first letter of a string', () => {
 
 test('Reverse string', () => {
     expect(reverseString('banana')).toBe('ananab');
+});
+
+test('Calculator', () => {
+    expect(calculator.add(5, 5)).toBe(10);
+    expect(calculator.subtract(5, 5)).toBe(0);
+    expect(calculator.divide(5, 5)).toBe(1);
+    expect(calculator.multiply(5, 5)).toBe(25);
+});
+
+test('Caesar Cipher', () => {
+    expect(caesarCipher('abc', 3)).toBe('def');
+    expect(caesarCipher('xyz', 3)).toBe('abc');
+    expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
+    expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
+});
+
+test('Analize Array', () => {
+    expect(analyzeArray([1, 8, 3, 4, 2, 6])).toStrictEqual({
+        average: 4,
+        min: 1,
+        max: 8,
+        length: 6,
+    });
 });
